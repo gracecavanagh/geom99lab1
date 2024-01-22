cfb60a68a91c8366187e7d74a39c316644409ad2
gracecavanagh <151089198+gracecavanagh@users.noreply.github.com>
2024-01-22 14:29:44 -0500

    Update clustering.js
    edited where the map opens to

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: { lat: -28.024, lng: 140.887 },
+    zoom: 5,
+    center: { lat: 46.944168, lng: -84.551017 },
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
