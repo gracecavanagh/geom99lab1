34430fdd3e2a8211c465247cbed023903f12ffaa
gracecavanagh <151089198+gracecavanagh@users.noreply.github.com>
2024-01-22 19:27:23 -0500

    Update europeantour.js

--- story/europeantour.js
+++ story/europeantour.js
@@ -150,21 +150,5 @@ function initMap() {
 
   map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
 }
-  const flightPlanCoordinates = [
-    { lat: 37.772, lng: -122.214 },
-    { lat: 21.291, lng: -157.821 },
-    { lat: -18.142, lng: 178.431 },
-    { lat: -27.467, lng: 153.027 },
-  ];
-  const flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: "#FF0000",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
-
-  flightPath.setMap(map);
-}
 
 window.initMap = initMap;
