ef9388a5f4227f89ebd591fca5d7bd19eae2404e
gracecavanagh <151089198+gracecavanagh@users.noreply.github.com>
2024-01-25 14:30:28 -0500

    Update europeantour.js

--- story/europeantour.js
+++ story/europeantour.js
@@ -2,18 +2,25 @@
 // focusable markers.
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
-    center: { lat: 34.84555, lng: -111.8035 },
+    zoom: 8,
+    center: { lat: 44.80500, lng: 10.32627 },
   });
   // Set LatLng and title text for the markers. The first marker (Boynton Pass)
   // receives the initial focus when tab is pressed. Use arrow keys to
   // move between markers; press tab again to cycle through the map controls.
   const tourStops = [
-    [{ lat: 34.8791806, lng: -111.8265049 }, "Boynton Pass"],
-    [{ lat: 34.8559195, lng: -111.7988186 }, "Airport Mesa"],
-    [{ lat: 34.832149, lng: -111.7695277 }, "Chapel of the Holy Cross"],
-    [{ lat: 34.823736, lng: -111.8001857 }, "Red Rock Crossing"],
-    [{ lat: 34.800326, lng: -111.7665047 }, "Bell Rock"],
+    [{ lat: 47.45312, lng: 8.56194 }, "Zurich Airport"],
+    [{ lat: 47.05171, lng: 8.30751 }, ""],
+    [{ lat: 47.05674, lng: 8.48580 }, ""],
+    [{ lat: 46.95659, lng: 8.51573 }, ""],
+    [{ lat: 46.00438, lng: 8.95270 }, ""],
+    [{ lat: 45.43947, lng: 10.99450 }, ""],
+    [{ lat: 45.44200, lng: 10.99867 }, ""],
+    [{ lat: 45.43799, lng: 12.33590 }, ""],
+    [{ lat: 45.45605, lng: 12.35194 }, ""],
+    [{ lat: 43.77144, lng: 11.25402 }, ""],
+    [{ lat: 41.90309, lng: 12.45449 }, ""],
+    [{ lat: 41.79940, lng: 12.59318 }, "Leonardo da Vinci–Fiumicino Airport"]
   ];
   // Create an info window to share between markers.
   const infoWindow = new google.maps.InfoWindow();
